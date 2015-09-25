522b72dab43d0d1a5a11b773baf61e62792941e2
Ivan Wang <icw@cmu.edu>
2015-09-25 16:22:29 -0400

    Add movement and base behavior scripts

---
+++ Assets/Scripts/BaseBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Base behavior class. Inherit this when implementing flocking, wander, etc.
+ */
+public abstract class BaseBehavior {
+    public float scale = 0;
+    public abstract Vector3 ComputeVelocity();
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Master class that combines movement vectors from various other scripts
+ */
+public class Movement : MonoBehaviour {
+    public BaseBehavior[] behaviors;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 newVelocity = Vector3.zero;
+        foreach (BaseBehavior behavior in behaviors) {
+            newVelocity += behavior.ComputeVelocity() * behavior.scale;
+        }
+
+        // TODO: set rigidbody acceleration or velocity?
+    }
+
+}
