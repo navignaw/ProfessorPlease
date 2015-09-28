e26cfabdc088c971512d5fd01f31a9c5effa5cea
Anna Etzel <aetzel@andrew.cmu.edu>
2015-09-28 10:38:15 -0400

    Square formation, with 1 leader and 8 followers with fixed positions relative to leader; positions rotate with leader rotation

---
+++ Assets/Scripts/Behaviors/SquareFormationFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Random wander behavior. TODO: Jun implement this
+ */
+public class SquareFormationFollow : BaseBehavior {
+	public Vector3 pos;
+	public GameObject leader;
+
+    public override Vector3 ComputeVelocity() {
+    	Vector3 baseRot = new Vector3(0.0f, 270.0f, 0.0f);
+    	Vector3 rot = leader.transform.rotation.eulerAngles - baseRot;
+        Quaternion qrot = Quaternion.Euler(rot.x, rot.y, rot.z);
+        Vector3 aim = (qrot * pos); // rotate it
+    	Vector3 v = (leader.transform.position + aim - this.transform.position)* 5.0f;
+        return v;
+    }
+
+}
---
+++ Assets/Scripts/SquareFormation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareFormation : MonoBehaviour {
+    public int size;
+    public GameObject model;
+    private GameObject[] formation;
+
+    // Use this for initialization
+    public void Start () {
+        formation = new GameObject[size];
+        int i;
+        GameObject floor = GameObject.Find("Floor");
+        for (i = 0; i < size; i++) {
+            Vector3 pos = new Vector3 (
+                Random.value * floor.transform.localScale.x,
+                0.0f,
+                Random.value * floor.transform.localScale.z
+                );
+            GameObject student = Instantiate(model, transform.position, transform.rotation) as GameObject;
+            student.transform.parent = transform;
+            student.transform.localPosition = pos;
+            formation[i] = student;
+        }
+    }
+    
+    // Update is called once per frame
+    public void Update () {
+        /*int i;
+        for (i = 0; i < size; i++) {
+            formation[i].transform.position = 
+            formation[i].transform.position - (formation[i].transform.position - transform.position) * 0.01f;
+        }*/
+    }
+
+}
