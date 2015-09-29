1141adc4fafde5ab5ea5df5bb5857f5496e431e2
Pretzelish <aetzel@andrew.cmu.edu>
2015-09-29 10:35:02 -0400

    VShape Formation, customizable angle

---
+++ Assets/Scripts/VShapeFormation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VShapeFormation : MonoBehaviour {
+    public float separation;
+    public float angle;
+    public GameObject[] formation;
+
+    // Use this for initialization
+    public void Start () {
+        int i;
+        Vector3 dir = Vector3.zero;
+        for (i = 0; i < formation.Length; i++) {
+            dir.x = 0.0f;
+            dir.z = -((i - (i % 2)) / 2) - 1;
+            if (i % 2 == 0) {
+                dir = Quaternion.Euler(0.0f, angle, 0.0f) * dir;
+            } else {
+                dir = Quaternion.Euler(0.0f, -angle, 0.0f) * dir;
+            }
+            formation[i].GetComponent<FormationFollow>().pos = 
+                separation * dir;
+            formation[i].GetComponent<FormationFollow>().leader = this.gameObject;
+        }
+    }
+
+}
