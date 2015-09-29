ed5e9ceff2e56df717e4eec3cc584cdc00a92837
Ivan Wang <icw@cmu.edu>
2015-09-28 22:54:49 -0400

    add ReachGoal behavior and scene

---
+++ Assets/Scripts/Behaviors/ReachGoal.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Move towards a target goal position.
+ */
+public class ReachGoal : BaseBehavior {
+    public float stopRadius = 3f;
+	public GameObject target;
+
+    public override Vector3 ComputeVelocity() {
+        // If close enough to target, stop moving
+        float distance = Vector3.Distance(target.transform.position, this.transform.position);
+        if (distance <= stopRadius) {
+            return Vector3.zero;
+        }
+
+        float scale = 1f;
+
+        // If approaching 10 * radius, start slowing down
+        if (distance <= 10 * stopRadius) {
+            scale *= Mathf.Max(0.25f, (distance - stopRadius) / (10 * stopRadius));
+        }
+
+    	Vector3 rot = target.transform.rotation.eulerAngles;
+        Quaternion qrot = Quaternion.Euler(rot.x, rot.y, rot.z);
+        Vector3 aim = qrot * Vector3.zero;
+    	Vector3 v = (target.transform.position + aim - this.transform.position) * scale;
+        return v;
+    }
+
+}
