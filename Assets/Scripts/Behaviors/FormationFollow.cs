e03e1f3f9134a64ea8e6250347bd21ed4ce3746a
Pretzelish <aetzel@andrew.cmu.edu>
2015-09-29 12:55:40 -0400

    fixing formation jitter

--- Assets/Scripts/Behaviors/FormationFollow.cs
+++ Assets/Scripts/Behaviors/FormationFollow.cs
@@ -5,14 +5,21 @@ using System.Collections;
  * Follow the leader in formation.
  */
 public class FormationFollow : BaseBehavior {
-	public Vector3 pos;
-	public GameObject leader;
+    public Vector3 pos;
+    public GameObject leader;
+    private Vector3 aimahead = new Vector3(0.0f, 0.0f, 0.75f);
 
+    /**
+     * returns velocity toward assigned position, taking into account
+     * the rotation of the leader and a need to aim slightly ahead of the
+     * assigned position to maintain a clean formation
+     */
     public override Vector3 ComputeVelocity() {
-    	Vector3 rot = leader.transform.rotation.eulerAngles;
-        Quaternion qrot = Quaternion.Euler(rot.x, rot.y, rot.z);
-        Vector3 aim = qrot * pos;
-    	Vector3 v = (leader.transform.position + aim - this.transform.position) * 5.0f;
+        Vector3 leaderRot = leader.transform.rotation.eulerAngles;
+        Quaternion qRot = Quaternion.Euler(leaderRot.x, leaderRot.y, leaderRot.z);
+        Vector3 aheadPos = pos + aimahead;
+        Vector3 aim = qRot * aheadPos;
+        Vector3 v = leader.transform.position + aim - this.transform.position;
         return v;
     }
 
