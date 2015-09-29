645f106a0d423a81ce89b89ab6a6b07e38154597
junhuo <jhuo@andrew.cmu.edu>
2015-09-29 15:45:00 -0400

    made all stages bigger, fixed up wander

--- Assets/Scripts/Behaviors/Wander.cs
+++ Assets/Scripts/Behaviors/Wander.cs
@@ -5,33 +5,44 @@ using System.Collections;
  * Random wander behavior. TODO: Jun implement this
  */
 public class Wander : BaseBehavior {
-	public float circleRadius = 0.5f;
-    public float wanderStrength = 1.2f;
+	public float circleRadius = 10.0f;
+    public float wanderStrength = 0.4f;
 	public float wanderAngle = 0.0f; // change to current direction
-	public float angleChange = 0.1f; // change in angle per frame
+	public float angleChange = 1f; // change in angle per frame
+
+    private Vector3 wander() {
+        // based on wander behavior proposed by Craig Reynolds
+
+        // get circle in front of model (velocity vector)
+        Vector3 circleCenter = this.transform.forward;
+        circleCenter.Normalize();
+        circleCenter = circleCenter*circleRadius;
+
+
+        // make new force vector for random wander
+        Vector3 displacement = new Vector3(0,0,0);
+        displacement.x = Mathf.Cos(wanderAngle);
+        displacement.z = Mathf.Sin(wanderAngle);
+        displacement.Normalize();
+        displacement = displacement*wanderStrength;
+
+        wanderAngle += (Random.value-0.5f)*angleChange;
+
+        Vector3 wanderForce = new Vector3();
+        wanderForce = circleCenter+displacement;
+
+        return wanderForce;
+    }
 
 	public override Vector3 ComputeVelocity()  {
-		// based on wander behavior proposed by Craig Reynolds
         float speed = this.transform.forward.magnitude;
-    	
-    	// get circle in front of model (velocity vector)
-    	Vector3 circleCenter = this.transform.forward;
-    	circleCenter.Normalize();
-    	circleCenter = circleCenter*circleRadius;
-
-
-    	// make new force vector for random wander
-    	Vector3 displacement = new Vector3(0,0,0);
-    	displacement.x = Mathf.Cos(wanderAngle)*wanderStrength;
-    	displacement.z = Mathf.Sin(wanderAngle)*wanderStrength;
-
-    	wanderAngle += (Random.value-0.5f)*angleChange;
 
     	// apply force
-    	Vector3 wanderForce = new Vector3();
-    	wanderForce = circleCenter+displacement;
+        Vector3 wanderForce = wander();
+        Vector3 forward = this.transform.forward;
+        forward.Normalize();
 
-        Vector3 total = 0.7f*this.transform.forward + 0.3f*wanderForce;
+        Vector3 total = forward + wanderForce;
         total.Normalize();
 
     	return speed*total;
