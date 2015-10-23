6be346650a88f793f85817a5661cddf5f21f5b8e
Pretzelish <aetzel@andrew.cmu.edu>
2015-10-23 15:55:21 -0400

    Graph class for Pathfinding

---
+++ Assets/Scripts/Pathfinding/Graph.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Graph class.
+ */
+public abstract class Graph {
+    public float dist;
+    public Vector3 startpos;
+    public Vector3 endpos;
+    // neighbors [row (x indexed)][col (z indexed)][index into neighbor list]
+    public Vector2[][][] neighbors;
+
+    Vector2 NearestNode(Vector3 pos) {
+    	return Vector2.zero;
+    }
+}
