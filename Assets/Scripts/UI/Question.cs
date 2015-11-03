50df98538c27fd91450f31c8ed754cbea130ef84
Ivan Wang <icw@cmu.edu>
2015-11-02 23:59:09 -0500

    randomize questions, add level 2 scene

--- Assets/Scripts/UI/Question.cs
+++ Assets/Scripts/UI/Question.cs
@@ -6,12 +6,11 @@ using System.Collections;
  */
 public class Question : MonoBehaviour {
     public GameObject messagePrefab;
-    public string messageText; // TODO: pull from array of random messages
     public float distance = 3f; // how far away before the question is asked
 
     private Transform canvas;
     private GameObject target;
-    private bool askedQuestion = false;
+    private bool askedQuestion = false; // TODO: add countdown before they ask another question
 
     // Use this for initialization
     void Start() {
@@ -39,10 +38,30 @@ public class Question : MonoBehaviour {
 
 
     private void AskQuestion() {
-        Vector3 offset = new Vector3((Random.value - 0.5f) * 800f, (Random.value - 0.5f) * 400f, 0f); // generate random offset
-        float duration = 3f + Random.value * 2f; // random duration between 3-5s
-        CreateMessage(messageText, offset, duration);
+        string text = Question.RandomQuestion;
+        Vector3 offset = new Vector3(Random.Range(-300f, 300f), Random.Range(-200f, 200f), 0f); // generate random offset
+        float duration = Random.Range(3f, 5f); // random duration between 3-5s
+        CreateMessage(text, offset, duration);
         askedQuestion = true;
+    }
+
+    private static string[] Questions = new string[] {
+        "Professor, will splay trees be on the midterm?",
+        "Professor, will infinite Ramsey theory be on the midterm?",
+        "Professor, will countable sets be on the midterm?",
+        "Professor, will unbounded fan-in circuits be on the midterm?",
+        "Professor, can I get an extension on the homework, please?",
+        "Professor, how many late days do I have left for this lab?",
+        "Professor, I'm so sorry but I forgot to turn in my homework yesterday! Can I please turn it in now?",
+        "Professor, I was deathly ill last night with hypothermia. Can I please get an extension?",
+        "Professor, I was super drunk at a party last night. Can I please get an extension?",
+        "Professor, are you single? My friend is really interested in taking you out to lunch...",
+    };
+
+    private static string RandomQuestion {
+        get {
+            return Questions[Random.Range(0, Questions.Length)];
+        }
     }
 
 }
