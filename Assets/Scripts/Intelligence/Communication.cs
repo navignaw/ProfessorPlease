fe2c62355ca2481d623ed885e4689b9d8653c067
Ivan Wang <icw@cmu.edu>
2015-11-19 16:28:59 -0500

    set up group members and pingGroup

--- Assets/Scripts/Intelligence/Communication.cs
+++ Assets/Scripts/Intelligence/Communication.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [System.Serializable]
 public enum StudentState {
@@ -12,13 +13,23 @@ public enum StudentState {
  * Communication script for intelligence module.
  */
 public class Communication : MonoBehaviour {
-    public int groupId;
+    public int groupId = 0;
     public StudentState state;
     public GameObject target;
     public Vector3 targetLastKnownPos = Vector3.zero;
 
+    private List<Communication> groupMembers;
+
     // Use this for initialization
     void Start() {
+        // Iterate through all students, add students with same groupId to groupMembers
+        groupMembers = new List<Communication>();
+        Communication[] allStudents = FindObjectsOfType(typeof(Communication)) as Communication[];
+        for (int i = 0; i < allStudents.Length; i++) {
+            if (allStudents[i] != this && allStudents[i].groupId == groupId) {
+                groupMembers.Add(allStudents[i]);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -32,6 +43,16 @@ public class Communication : MonoBehaviour {
 
             case StudentState.Seeking:
                 break;
+        }
+    }
+
+    // message all groups with target's location
+    void PingGroup(Vector3 targetPos) {
+        foreach (Communication groupMember in groupMembers) {
+            if (groupMember.state != StudentState.Follow) {
+                groupMember.targetLastKnownPos = targetPos;
+                groupMember.state = StudentState.Seeking;
+            }
         }
     }
 
