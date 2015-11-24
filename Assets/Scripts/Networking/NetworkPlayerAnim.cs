f246990ecb30510c888409a6605f45147aae5589
Ivan Wang <icw@cmu.edu>
2015-11-23 22:54:28 -0500

    fix player animation sync

---
+++ Assets/Scripts/Networking/NetworkPlayerAnim.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent (typeof(NetworkAnimator))]
+public class NetworkPlayerAnim : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        NetworkAnimator anim = GetComponent<NetworkAnimator>();
+        anim.SetParameterAutoSend(0, true); // forward
+        anim.SetParameterAutoSend(1, true); // turn
+        anim.SetParameterAutoSend(3, true); // on ground
+        anim.SetParameterAutoSend(4, true); // jump
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
